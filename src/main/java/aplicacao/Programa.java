a153e7c537168dae792b10977d46c1230472359a
João <jfmoraeslara@gmail.com>
2022-09-03 13:02:04 -0300

    Nivelamento Java JPA

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,45 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		/* inserindo dados		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "carlos@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "carlos@gmail.com");
+		*/
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		/* pegando os dados inseridos os dados
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		*/
+		
+		/* buscar uma pessoal pelo id
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);*/
+		
+		/* removendo dado do banco
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		*/	
+		
+		System.out.println("Pronto");
+		emf.close();
+		em.close();
+	}
+}
